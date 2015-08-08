43098a0f59b2d28fba5875a7a8f9e751938b1123
liao <gonsakon@gmail.com>
2015-08-08 18:58:53 +0800

    change api url

--- javascripts/all.js
+++ javascripts/all.js
@@ -10,7 +10,7 @@ app.controller('PeopleCtrl', function($scope,$http) {
     setTimeout(function(){ google.maps.event.trigger(map, 'resize'); }, 1000);
   };
   $scope.taipeiArea=['中正區','大同區','中山區','松山區','大安區','萬華區','信義區','士林區','北投區','內湖區','南港區','文山區'];
-  $http.get('https://tcgbusfs.blob.core.windows.net/blobfs/GetDisasterSummary.json').
+  $http.get('http://tonyq.org/kptaipei/GetDisasterSummary-20150808.php').
   success(function(data, status, headers, config) {
     $scope.streetData = data.DataSet["diffgr:diffgram"][0].NewDataSet[0].CASE_SUMMARY;
     
@@ -20,7 +20,7 @@ app.controller('PeopleCtrl', function($scope,$http) {
   });
 var mapOptions = { zoom: 15, center: new google.maps.LatLng(25.042355, 121.532904) };
 var map = new google.maps.Map( document.getElementById('mapCanvas') , mapOptions);
-d3.json("https://tcgbusfs.blob.core.windows.net/blobfs/GetDisasterSummary.json", function(data){
+d3.json("http://tonyq.org/kptaipei/GetDisasterSummary-20150808.php", function(data){
 var mapdata = data.DataSet["diffgr:diffgram"][0].NewDataSet[0].CASE_SUMMARY;
   var overlay = new google.maps.OverlayView();
 
